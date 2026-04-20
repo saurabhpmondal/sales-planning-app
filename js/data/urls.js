cda2f45611fe1b5d019a33e27bc71fdf6a83f5f3
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-20 13:35:19 +0530

    Update urls.js

--- js/data/urls.js
+++ js/data/urls.js
@@ -1,1 +1,41 @@
-// js/data/urls.js
+// FILE: js/data/urls.js
+
+/* -----------------------------------
+   GOOGLE SHEETS CSV URLS
+   Locked from user provided sources
+----------------------------------- */
+
+export const DATA_URLS = {
+  saleData:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1679615114&single=true&output=csv",
+
+  returnData:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1201655010&single=true&output=csv",
+
+  traffic:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=533529379&single=true&output=csv",
+
+  sjitStock:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=685171659&single=true&output=csv",
+
+  sorStock:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=2104491192&single=true&output=csv",
+
+  sellerStock:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=325497638&single=true&output=csv",
+
+  productMaster:
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=205952585&single=true&output=csv"
+};
+
+/* -----------------------------------
+   HELPERS
+----------------------------------- */
+
+export function getAllDataUrls() {
+  return DATA_URLS;
+}
+
+export function getDataUrl(key) {
+  return DATA_URLS[key] || "";
+}
