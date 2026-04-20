fc3cd9f4530bcf206df1886fb84dce4996a165f7
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-20 13:29:39 +0530

    Update appConfig.js

--- js/config/appConfig.js
+++ js/config/appConfig.js
@@ -1,1 +1,67 @@
-// js/config/appConfig.js
+// FILE: js/config/appConfig.js
+
+import { APP_INFO } from "../core/constants.js";
+
+/* -----------------------------------
+   MASTER APP CONFIG
+----------------------------------- */
+
+export const APP_CONFIG = {
+  /* Identity */
+  name: APP_INFO.name,
+  version: APP_INFO.version,
+
+  /* Theme */
+  theme: {
+    defaultMode: "light",
+    allowDarkMode: false
+  },
+
+  /* Layout */
+  layout: {
+    maxWidth: 1600,
+    stickyHeader: true,
+    stickyFilters: true,
+    stickyTabs: true
+  },
+
+  /* Data */
+  data: {
+    autoLoadOnStart: true,
+    enableCache: true,
+    retryCount: 2
+  },
+
+  /* Reports */
+  reports: {
+    defaultTab: "dashboard",
+    enableExports: true
+  },
+
+  /* Search */
+  search: {
+    enabled: true,
+    keys: ["styleId", "erpSku"]
+  },
+
+  /* Formatting */
+  format: {
+    currency: "INR",
+    locale: "en-IN",
+    decimals: 2
+  },
+
+  /* Debug */
+  debug: {
+    enabled: false,
+    consoleLogs: false
+  }
+};
+
+/* -----------------------------------
+   HELPERS
+----------------------------------- */
+
+export function getAppConfig() {
+  return APP_CONFIG;
+}
