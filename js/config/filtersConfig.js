abbd2e254c5d2ca6f33174bf9dae0fc2e7aa8e07
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-20 13:33:06 +0530

    Update filtersConfig.js

--- js/config/filtersConfig.js
+++ js/config/filtersConfig.js
@@ -1,1 +1,62 @@
-// js/config/filtersConfig.js
+// FILE: js/config/filtersConfig.js
+
+/* -----------------------------------
+   GLOBAL FILTER CONFIG
+----------------------------------- */
+
+export const FILTERS_CONFIG = [
+  {
+    key: "month",
+    label: "Month",
+    type: "select",
+    defaultValue: "ALL",
+    dynamic: true
+  },
+  {
+    key: "startDate",
+    label: "Start Date",
+    type: "date",
+    defaultValue: ""
+  },
+  {
+    key: "endDate",
+    label: "End Date",
+    type: "date",
+    defaultValue: ""
+  },
+  {
+    key: "brand",
+    label: "Brand",
+    type: "select",
+    defaultValue: "ALL",
+    dynamic: true
+  },
+  {
+    key: "poType",
+    label: "PO Type",
+    type: "select",
+    defaultValue: "ALL",
+    dynamic: false,
+    options: [
+      "ALL",
+      "FC",
+      "PPMP",
+      "SJIT",
+      "SOR"
+    ]
+  },
+  {
+    key: "search",
+    label: "Search",
+    type: "search",
+    placeholder: "Search style ID / ERP SKU"
+  }
+];
+
+/* -----------------------------------
+   HELPERS
+----------------------------------- */
+
+export function getFiltersConfig() {
+  return FILTERS_CONFIG;
+}
