74a2bc8c0cd9c2b2348da143c34dcc7c9eb91a01
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-20 14:31:48 +0530

    Create dataSources.js

---
+++ js/config/dataSources.js
@@ -0,0 +1,58 @@
+// NEW FILE
+// FILE: js/config/dataSources.js
+
+/* -----------------------------------
+   GOOGLE SHEETS CSV SOURCES
+   Central source registry
+----------------------------------- */
+
+export const DATA_SOURCES = [
+  {
+    key: "saleData",
+    label: "Sales",
+    url:
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1679615114&single=true&output=csv"
+  },
+
+  {
+    key: "returnData",
+    label: "Returns",
+    url:
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=1201655010&single=true&output=csv"
+  },
+
+  {
+    key: "traffic",
+    label: "Traffic",
+    url:
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=533529379&single=true&output=csv"
+  },
+
+  {
+    key: "sjitStock",
+    label: "SJIT Stock",
+    url:
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=685171659&single=true&output=csv"
+  },
+
+  {
+    key: "sorStock",
+    label: "SOR Stock",
+    url:
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=2104491192&single=true&output=csv"
+  },
+
+  {
+    key: "sellerStock",
+    label: "Seller Stock",
+    url:
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=325497638&single=true&output=csv"
+  },
+
+  {
+    key: "productMaster",
+    label: "Product Master",
+    url:
+      "https://docs.google.com/spreadsheets/d/e/2PACX-1vTGOsj66mo-CpS5eTerQgEcjYvr5GuOkQUIQ_9Sy4bwFu6FjGv9wBvCZn5UQBcFB7M-dcuJdbxMxSnj/pub?gid=205952585&single=true&output=csv"
+  }
+];
