f5bcb6fe6f95317fdd351745f71c5ae236d8d314
saurabhpmondal <saurabhpmondal@gmail.com>
2026-04-20 13:39:27 +0530

    Update searchBox.js

--- js/components/searchBox.js
+++ js/components/searchBox.js
@@ -1,1 +1,46 @@
-// js/components/searchBox.js
+// FILE: js/components/searchBox.js
+
+/* -----------------------------------
+   REUSABLE SEARCH BOX COMPONENT
+   Can be used inside reports later
+----------------------------------- */
+
+export function createSearchBox({
+  placeholder = "Search...",
+  value = "",
+  filterKey = "search"
+} = {}) {
+  const wrapper =
+    document.createElement("div");
+
+  wrapper.className =
+    "search-wrap";
+
+  wrapper.innerHTML = `
+    <input
+      type="text"
+      class="search-input"
+      data-filter="${filterKey}"
+      placeholder="${placeholder}"
+      value="${escapeHtml(value)}"
+    />
+
+    <span class="search-icon">
+      🔍
+    </span>
+  `;
+
+  return wrapper;
+}
+
+/* -----------------------------------
+   HELPERS
+----------------------------------- */
+
+function escapeHtml(value = "") {
+  return String(value)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;");
+}
